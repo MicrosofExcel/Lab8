72fc4dcfa3abb929e67b0e20d5f4b5ba201c5319
Excel Ojeifo <eojeifo@ualberta.ca>
2024-10-22 17:39:16 -0600

    Commit and push test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import androidx.appcompat.view.menu.MenuBuilder;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +14,44 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    /**
+     * this gets size of the list
+     * @return
+     */
+    public int getCount(){
+        return cities.size();
+    }
+    /**
+     * this adds a city object to the list
+     *for the first phase it will be
+     empty * @param city
+     */
+    public void addCity(City city){
+    }
+
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return
+     */
+
+    public CustomList MockCityList(){
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size
+     plus one
+     */
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
 
 }
